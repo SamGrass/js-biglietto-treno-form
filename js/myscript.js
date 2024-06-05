f7d45bf865302f4f46a5f782a572998fca53a787
Samgrass <samuelegrassetti01@gmail.com>
2024-06-05 17:17:05 +0200

    add myButton function for name and Km

--- js/myscript.js
+++ js/myscript.js
@@ -1,17 +1,29 @@
 // chiedere nome e cognome
 const userName = document.querySelector("#nome-cognome");
 //chiedere all’utente il numero di chilometri che vuole percorrere
-const userKm = parseFloat(document.querySelector("#Kilometri"));
+const userKm = document.querySelector("#Kilometri");
 //l’età del passeggero.
 // const userAge = document.querySelecto("#selezione-età");
 
 console.log(userName, userKm,)
-// creare un pulsante che aggiorni nome, Km, età
-const myButton = document.querySelector("#my-button")
-//calcolare prezzo biglietto in base ai km (0.21 € al km)
-let pricePerKm = userKm * 0.21;
 
-console.log(pricePerKm);
+// creare un pulsante che aggiorni nome, Km, età
+let inputKm;
+const myButton = document.querySelector("#my-button");
+
+ myButton.addEventListener("click",
+    function(){
+        let inputName = userName.value;
+        let inputKm = parseFloat(userKm.value);
+        let pricePerKm = inputKm * 0.21;
+        console.log(pricePerKm, inputName);
+     }
+    
+ );
+
+
+//calcolare prezzo biglietto in base ai km (0.21 € al km)
+let pricePerKm;
 
 let totalPrice = pricePerKm;
 
@@ -22,14 +34,14 @@ let minorsDiscount = 80 / 100;
 let eldersDiscount = 60 / 100;
 
 // cambiare il prezzo totale in base all'età
-// // if (userAge <= 17) {
+// // if (userAge === "minor") {
 // //     totalPrice = pricePerKm * minorsDiscount;
 // // }
-// // else if (userAge >= 65) {
+// // else if (userAge === "elder") {
 // //     totalPrice = pricePerKm * eldersDiscount;
 // // }
 // // console.log(totalPrice);
 
 
 // rendere il risultato 2 digit decimali per 
-let visiblePrice = totalPrice.toFixed(2);
+// // let visiblePrice = totalPrice.toFixed(2);
